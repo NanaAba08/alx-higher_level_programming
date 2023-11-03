08f1642f89c75bae6ce04fe6af84bd598a6eb808
NanaAba08 <williamsnanaaba@gmail.com>
2023-11-03 04:40:43 -0700

    a Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message fix

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Make a POST request to the specified URL with a custom User-Agent header
-curl -s -X PUT -H "User-Agent: You_got_me" 0.0.0.0:5000/catch_me
+curl -s -X PUT -L -d "user_id=98" 0.0.0.0:5000/catch_me
